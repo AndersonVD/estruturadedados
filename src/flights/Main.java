07f069f3dc97ec80a47c1f87e7d6d3e33bb56c11
Anderson Viana <anderson1565@live.com>
2023-04-17 11:26:50 -0300

    flights class created

--- src/flights/Main.java
+++ src/flights/Main.java
@@ -2,7 +2,9 @@ package flights;
 
 public class Main {
     static int tempo = 20;
-    int combustivel = 20;
+    static int combustivel = 20;
+    static int decolagem = 10;
+    static int aterrisasagem = 10;
 
     public static void main(String[] args) {
         System.out.println("Sucesso!");
